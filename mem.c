a3ba154671a77d72b23b576a23934a11148979a6
koliupy <srosa011@fiu.edu>
2017-11-14 17:58:21 -0500

    Update mem.c

--- mem.c
+++ mem.c
@@ -10,54 +10,8 @@
 //MEM_POLICY_FIRSTFIT 0
 //MEM_POLICY_BESTFIT  1
 //MEM_POLICY_WORSTFIT 2
-typedef struct allocated_memory {
-	int size;
-	int type;
-	int policy;
-	struct allocated_memory *next;
-}alloc_mem;
-int mem() {
-alloc_mem *pointer;
-
-    int Mem_Init(int size, int policy) {
-	// open the /dev/zero device
-	int fd = open("/dev/zero", O_RDWR);
-	// size (in bytes) must be divisible by page size
-	pointer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-	if (pointer == MAP_FAILED) {
-	   perror("mmap");
-	   return -1;
-	}
-
-	// close the device (don't worry, mapping should be unaffected)
-	close(fd);
-	(*pointer).size = size;
-	(*pointer).policy = policy;
-	return 0;
-    }
-
-    void* Mem_Alloc(int size) {
-	
-    }
-
-    int Mem_Free(void* ptr) {
-	
-    }
-
-    int Mem_IsValid(void* ptr) {
-
-    }#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include "mem.h"
-
-//Defines from mem.h:
-//MEM_POLICY_FIRSTFIT 0
-//MEM_POLICY_BESTFIT  1
-//MEM_POLICY_WORSTFIT 2
+#define MEM_TYPE_FREE 0
+#define MEM_TYPE_ALLOC 1
 
 typedef struct memory {
 	int size;
@@ -65,7 +19,7 @@ typedef struct memory {
 	struct memory *next;
 }mem;
 
-int static policy = -1;
+int static mem_policy = -1;
 mem *pointer = NULL;
 
 int Mem_Init(int size, int policy)
@@ -74,33 +28,46 @@ int Mem_Init(int size, int policy)
 	{
 		return -1;
 	}
-	if ((policy < 0) || (policy > 2))
+	if ((policy < MEM_POLICY_FIRSTFIT) || (policy > MEM_POLICY_WORSTFIT))
 	{
 		return -1;
 	}
-	
-
-
-
-
-
-
-
-
-	// open the /dev/zero device
-	int fd = open("/dev/zero", O_RDWR);
-	// size (in bytes) must be divisible by page size
-	pointer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-	if (pointer == MAP_FAILED)
+	else
 	{
-	   perror("mmap");
-	   return -1;
+		mem_policy = policy;
 	}
 
-	// close the device (don't worry, mapping should be unaffected)
-	close(fd);
-	(*pointer).size = size;
-	(*pointer).policy = policy;
+	// align size with page size
+	int page_size = getpagesize();
+
+	printf("Size of region = %d\n", size);
+	printf("Page size = %d\n", page_size);
+
+	int diff = size % page_size;
+	if (diff != 0)
+	{
+		size += (page_size - diff);
+	}
+
+	printf("Size of region to be allocated = %d\n", size);
+
+	// memory chunk
+
+	int fd = open("/dev/zero", O_RDWR);
+	head_node = (mem *) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+
+	if (head_node == NULL)
+	{
+		return -1;
+	}
+
+	close (fd);
+	head_node->size = size - sizeof(mem);
+	head_node->type =
+	head_node->next = NULL;
+
+	printf("memory chunk address: %8x\nsize = %d\n", head_node, head_node->size);
+
 	return 0;
 }
 
@@ -121,16 +88,5 @@ int Mem_GetSize(void* ptr) {
 }
 
 float Mem_GetFragmentation() {
-
-}
-
-
-    int Mem_GetSize(void* ptr) {
-
-    }
-
-    float Mem_GetFragmentation() {
-
-    }
 
 }
