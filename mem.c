0baada18c7cf8d7929780767939ee64cd344c9b2
koliupy <srosa011@fiu.edu>
2017-11-17 11:09:33 -0500

    Fixed free fragment merge

--- mem.c
+++ mem.c
@@ -21,6 +21,7 @@ typedef struct memory {
 
 typedef struct header {
 	int freeSize;
+	struct memory *previous;
 	struct memory *current;
 	struct memory *found;
 	struct memory *first;
@@ -106,7 +107,7 @@ void* Mem_Alloc(int size)
 				}
 			}
 		}
-		// Finds the smallest valid free fragmen
+		// Finds the smallest valid free fragment
 		else if (mem_policy == MEM_POLICY_BESTFIT)
 		{
 			if (pointer->current->type == MEM_TYPE_FREE)
@@ -146,7 +147,7 @@ void* Mem_Alloc(int size)
 	// Allocate memory and change free memory size
 	pointer->current = pointer->found->next;
 	pointer->found->type = MEM_TYPE_ALLOC;
-	pointer->found->next = pointer->found + size + 1;
+	pointer->found->next = pointer->found + size;
 	pointer->found->next->type = MEM_TYPE_FREE;
 	pointer->found->next->next = pointer->current;
 	pointer->found->next->size = pointer->found->size - size;
@@ -161,19 +162,72 @@ int Mem_Free(void* ptr)
 	{
 		return -1;
 	}
+	pointer->previous = NULL;
 	pointer->current = pointer->first;
 	while (pointer->current != NULL)
 	{
-		if (pointer->current->type == MEM_TYPE_ALLOC && (mem *)ptr >= pointer->current && (mem *)ptr <= pointer->current + pointer->current->size)
+		if (pointer->current->type == MEM_TYPE_ALLOC)
 		{
-			pointer->current->type = MEM_TYPE_FREE;
-			pointer->freeSize += pointer->current->size;
-			return 0;
+			if (pointer->current->next == NULL)
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr <= pointer->current + pointer->current->size)
+				{
+					pointer->current->type = MEM_TYPE_FREE;
+					pointer->freeSize += pointer->current->size;
+
+					if (pointer->previous != NULL)
+					{
+						if (pointer->previous->type == MEM_TYPE_FREE)
+						{
+							pointer->previous->size += pointer->current->size;
+							pointer->previous->next = pointer->current->next;
+						}
+					}
+					return 0;
+				}
+			}
+			else
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr < pointer->current->next)
+				{
+					pointer->current->type = MEM_TYPE_FREE;
+					pointer->freeSize += pointer->current->size;
+
+					if (pointer->current->next->type == MEM_TYPE_FREE)
+					{
+						pointer->current->size += pointer->current->next->size;
+						pointer->current->next = pointer->current->next->next;
+					}
+					if (pointer->previous != NULL)
+					{
+						if (pointer->previous->type == MEM_TYPE_FREE)
+						{
+							pointer->previous->size += pointer->current->size;
+							pointer->previous->next = pointer->current->next;
+						}
+					}
+					return 0;
+				}
+			}
 		}
-		else if ((pointer->current->type == MEM_TYPE_FREE) && ((mem *)ptr >= pointer->current && (mem *)ptr <= (pointer->current + pointer->current->size)))
+		else if (pointer->current->type == MEM_TYPE_FREE)
 		{
-			return -1;
+			if (pointer->current->next == NULL)
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr <= pointer->current + pointer->current->size)
+				{
+					return -1;
+				}
+			}
+			else
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr < pointer->current->next)
+				{
+					return -1;
+				}
+			}
 		}
+		pointer->previous = pointer->current;
 		pointer->current = pointer->current->next;
 	}
 	return -1;
@@ -184,13 +238,39 @@ int Mem_IsValid(void* ptr)
 	pointer->current = pointer->first;
 	while (pointer->current != NULL)
 	{
-		if ((pointer->current->type == MEM_TYPE_ALLOC) && ((mem *)ptr >= pointer->current && (mem *)ptr <= (pointer->current + pointer->current->size)))
+		if (pointer->current->type == MEM_TYPE_ALLOC)
 		{
-			return 1;
+			if (pointer->current->next == NULL)
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr <= pointer->current + pointer->current->size)
+				{
+					return 1;
+				}
+			}
+			else
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr < pointer->current->next)
+				{
+					return 1;
+				}
+			}
 		}
-		else if ((pointer->current->type == MEM_TYPE_FREE) && ((mem *)ptr >= pointer->current && (mem *)ptr <= (pointer->current + pointer->current->size)))
+		else if (pointer->current->type == MEM_TYPE_FREE)
 		{
-			return 0;
+			if (pointer->current->next == NULL)
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr <= pointer->current + pointer->current->size)
+				{
+					return 0;
+				}
+			}
+			else
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr < pointer->current->next)
+				{
+					return 0;
+				}
+			}
 		}
 		pointer->current = pointer->current->next;
 	}
@@ -202,13 +282,39 @@ int Mem_GetSize(void* ptr)
 	pointer->current = pointer->first;
 	while (pointer->current != NULL)
 	{
-		if ((pointer->current->type == MEM_TYPE_ALLOC) && ((mem *)ptr >= pointer->current && (mem *)ptr <= (pointer->current + pointer->current->size)))
+		if (pointer->current->type == MEM_TYPE_ALLOC)
 		{
-			return pointer->current->size;
+			if (pointer->current->next == NULL)
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr <= pointer->current + pointer->current->size)
+				{
+					return pointer->current->size;
+				}
+			}
+			else
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr < pointer->current->next)
+				{
+					return pointer->current->size;
+				}
+			}
 		}
-		else if ((pointer->current->type == MEM_TYPE_FREE) && ((mem *)ptr >= pointer->current && (mem *)ptr <= (pointer->current + pointer->current->size)))
+		else if (pointer->current->type == MEM_TYPE_FREE)
 		{
-			return -1;
+			if (pointer->current->next == NULL)
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr <= pointer->current + pointer->current->size)
+				{
+					return -1;
+				}
+			}
+			else
+			{
+				if ((mem *)ptr >= pointer->current && (mem *)ptr < pointer->current->next)
+				{
+					return -1;
+				}
+			}
 		}
 		pointer->current = pointer->current->next;
 	}
@@ -231,5 +337,5 @@ float Mem_GetFragmentation()
 		}
 		pointer->current = pointer->current->next;
 	}
-	return ((float)pointer->found->size/((float)pointer->freeSize));
+	return ((float)pointer->found->size/(float)pointer->freeSize);
 }
